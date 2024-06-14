a01b2cd5002453cfe6288ea74d8c67384f7090a0
MGorostidi <mgorostidiaicua@gmail.com>
2024-06-14 11:44:13 +0200

    Automatize variable initialization

--- initialize_parameters.sh
+++ initialize_parameters.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Intiialize terminal colors: 
 LIGHTCYAN='\033[1;36m'
 RED='\033[0;31m'
@@ -10,9 +9,30 @@ ORANGE="\033[0;33m"
 NC='\033[0m' # No Color
 
 
-# Define folder path to MBIOME project: 
-DIR="/home/unidad/Escritorio/MiriamGorostidi/aMICROBIOTA/mbiome"
-DIR_CONDA="/home/unidad/miniconda3/etc/profile.d/conda.sh"
+# Identify Mbiome folder: 
+DIR="$(pwd)"
+
+if [[ "$DIR" != *"mbiome" ]]; then
+    echo "Your current folder is not the "mbiome" folder: $DIR"
+    while read -ep "Now, please, enter the exact path folder where "mbiome" is located:" DIR_USER; do
+        if [[ "$DIR_USER" != *"mbiome" ]]; then
+            echo -e "Your current folder is not the "mbiome" folder"
+        else
+            echo -e "Perfect! Now you are in the "mbiom" folder!"
+            cd ${DIR_USER}
+            DIR=$DIR_USER
+            break
+        fi
+    done
+fi
+
+# Identify Conda folder: 
+conda_dir=$(which conda)
+conda_dir=${conda_dir//\/bin\/conda}
+DIR_CONDA=${conda_dir}/etc/profile.d/conda.sh
+
+
+QIIME2_ENV_NAME=qiime2-amplicon-2024.2
 
 if [ ! -d "$DIR/DATABASES" ]; then
     # Create folder if this one does not exist
@@ -28,5 +48,3 @@ if [ ! -d "$DIR/EXPERIMENTS" ]; then
 fi
 
 DIR_EXPERIMENTS=$DIR/EXPERIMENTS
-
-QIIME2_ENV_NAME=qiime2-amplicon-2024.2
