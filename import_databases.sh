635a60a4098c20743654b8eda3d3e4135582d2eb
MGorostidi <mgorostidiaicua@gmail.com>
2024-10-23 15:44:26 +0200

    Modify GG identity perc from 97 to 99

--- import_databases.sh
+++ import_databases.sh
@@ -23,42 +23,43 @@ conda activate $QIIME2_ENV_NAME
 
 
 # GREEN GENES DATABASE FOR 16S:
-echo -e "${PURPLE} Importing Green Genes database to Qiime2...${WHITE}"
+echo -e "${PURPLE} Importing Green Genes database to Qiime2, identity percentage of: $IDENTITY_PERC...${WHITE}"
+IDENTITY_PERC=99
 
-qiime tools import \
-  --input-path  $DIR_DATABASES/GreenGenes_DB/$GGfolder/trees/97_otus.tree \
-  --output-path $DIR_DATABASES/GreenGenes_DB/$GGfolder/97_otus_rooted-tree.qza \
-  --type 'Phylogeny[Rooted]'
+# qiime tools import \
+#   --input-path  $DIR_DATABASES/GreenGenes_DB/$GGfolder/trees/${IDENTITY_PERC}_otus.tree \
+#   --output-path $DIR_DATABASES/GreenGenes_DB/$GGfolder/${IDENTITY_PERC}_otus_rooted-tree.qza \
+#   --type 'Phylogeny[Rooted]'
 
 qiime tools import \
 --type 'FeatureData[Sequence]' \
---input-path $DIR_DATABASES/GreenGenes_DB/$GGfolder/rep_set/97_otus.fasta \
---output-path $DIR_DATABASES/GreenGenes_DB/$GGfolder/97_otus_refs.qza
+--input-path $DIR_DATABASES/GreenGenes_DB/$GGfolder/rep_set/${IDENTITY_PERC}_otus.fasta \
+--output-path $DIR_DATABASES/GreenGenes_DB/$GGfolder/${IDENTITY_PERC}_otus_refs.qza
 
 qiime tools import \
 --type 'FeatureData[Taxonomy]' \
 --input-format HeaderlessTSVTaxonomyFormat \
---input-path $DIR_DATABASES/GreenGenes_DB/$GGfolder/taxonomy/97_otu_taxonomy.txt \
---output-path $DIR_DATABASES/GreenGenes_DB/$GGfolder/97_otu_taxonomy.qza
+--input-path $DIR_DATABASES/GreenGenes_DB/$GGfolder/taxonomy/${IDENTITY_PERC}_otu_taxonomy.txt \
+--output-path $DIR_DATABASES/GreenGenes_DB/$GGfolder/${IDENTITY_PERC}_otu_taxonomy.qza
 
-# UNITE DATABASE FOR ITS:
-echo -e "${PURPLE} Importing UNITE database to Qiime2...${WHITE}"
 
-qiime tools import \
---type 'FeatureData[Sequence]' \
---input-path $DIR_DATABASES/UNITE_DB/$UNITEdynamicFasta.fasta \
---output-path $DIR_DATABASES/UNITE_DB/unite_dyn_refs.qza
+# # UNITE DATABASE FOR ITS:
+# echo -e "${PURPLE} Importing UNITE database to Qiime2...${WHITE}"
+# qiime tools import \
+# --type 'FeatureData[Sequence]' \
+# --input-path $DIR_DATABASES/UNITE_DB/$UNITEdynamicFasta.fasta \
+# --output-path $DIR_DATABASES/UNITE_DB/unite_dyn_refs.qza
 
-qiime tools import \
---type 'FeatureData[Taxonomy]' \
---input-format HeaderlessTSVTaxonomyFormat \
---input-path $DIR_DATABASES/UNITE_DB/$UNITEdynamicTXT.txt \
---output-path $DIR_DATABASES/UNITE_DB/unite_dyn_taxa.qza
+# qiime tools import \
+# --type 'FeatureData[Taxonomy]' \
+# --input-format HeaderlessTSVTaxonomyFormat \
+# --input-path $DIR_DATABASES/UNITE_DB/$UNITEdynamicTXT.txt \
+# --output-path $DIR_DATABASES/UNITE_DB/unite_dyn_taxa.qza
 
-qiime feature-classifier fit-classifier-naive-bayes \
---i-reference-reads $DIR_DATABASES/UNITE_DB/unite_dyn_refs.qza \
---i-reference-taxonomy $DIR_DATABASES/UNITE_DB/unite_dyn_taxa.qza \
---o-classifier $DIR_DATABASES/UNITE_DB/unite_classifier.qza
+# qiime feature-classifier fit-classifier-naive-bayes \
+# --i-reference-reads $DIR_DATABASES/UNITE_DB/unite_dyn_refs.qza \
+# --i-reference-taxonomy $DIR_DATABASES/UNITE_DB/unite_dyn_taxa.qza \
+# --o-classifier $DIR_DATABASES/UNITE_DB/unite_classifier.qza
 
 
 
