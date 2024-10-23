c6cbf85bd8dd669a3d23b228382dc8a84089feea
MGorostidi <mgorostidiaicua@gmail.com>
2024-10-23 15:44:03 +0200

    Adding Silva DB

--- download_databases.sh
+++ download_databases.sh
@@ -8,6 +8,14 @@ wget -O $DIR_DATABASES/gg_13_8_otus.tar.gz ftp://greengenes.microbio.me/greengen
 mkdir -p $DIR_DATABASES/GreenGenes_DB && tar xvzf $DIR_DATABASES/gg_13_8_otus.tar.gz -C $DIR_DATABASES/GreenGenes_DB
 
 
+# SILVA DATABASE FOR 16S: 
+# from: https://docs.qiime2.org/2024.5/data-resources/
+# Citation: bioRxiv 2020.10.05.326504; doi: https://doi.org/10.1101/2020.10.05.326504
+mkdir -p $DIR_DATABASES/Silva_DB
+echo -e "${PURPLE} Downloading Sivla database...${WHITE}"
+wget -O $DIR_DATABASES/Silva_DB/silva-138-99-tax.qza https://data.qiime2.org/2024.5/common/silva-138-99-tax.qza
+wget -O $DIR_DATABASES/Silva_DB/silva-138-99-seqs.qza https://data.qiime2.org/2024.5/common/silva-138-99-seqs.qza
+
 #UNITE DATABASE FOR ITS:
 echo -e "${PURPLE} Downloading Unite database...${WHITE}"
 UNITE_DB=$(ls $DIR_DATABASES/sh_qiime_release_*)
