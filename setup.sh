d60a4bdef1bd73d06b0c07e152b628dce4bcebb7
MGorostidi <mgorostidiaicua@gmail.com>
2024-10-30 15:56:24 +0100

    Editing SETUP so the environments and packages are installed and created by running it

--- setup.sh
+++ setup.sh
@@ -14,14 +14,41 @@ conda install wget
 
 # Download .yml file and create qiime2 environment:
 echo -e "${Purple} Downloading Qiime2 and creating conda environment... ${White}"
-#### MODIFY THE .yml Link:
-wget https://data.qiime2.org/distro/amplicon/qiime2-amplicon-2024.2-py38-linux-conda.yml
+conda env create -n qiime2-amplicon-2024.5 --file https://data.qiime2.org/distro/amplicon/qiime2-amplicon-2024.5-py39-linux-conda.yml
 
-conda env create -n qiime2-amplicon-2024.2 --file qiime2-amplicon-2024.2-py38-linux-conda.yml
-# remove .yml file
-rm qiime2-amplicon-2024.2-py38-linux-conda.yml
+# Download Metagenomics PP plugin by Thermofisher: (already available when cloning Mbiome repository)
+# From https://apps.thermofisher.com/apps/spa/#/publiclib/plugins , after loggin in in the Thermofisher account.
+# 1) Log in (or Sign in) in Thermofisher.
+# 2) Go to https://apps.thermofisher.com/apps/spa/#/publiclib/plugins 
+# 3) Search for MetagenomicsPP and download it
+# 4) Unzip the downloaded .zip folder
+# 5) Move the MetagenomicsPP folder to the mbiome folder. 
 
+
+# Downloading and Installing Sidle for reconstruction: 
+# Tutorial: https://q2-sidle.readthedocs.io/en/latest/database_preparation.html
+echo -e "${Purple} Downloading and Installing Sidle for reconstruction... ${White}"
+conda activate qiime2-amplicon-2024.5
+conda install dask
+conda install -c conda-forge -c bioconda -c qiime2 -c defaults xmltodict
+# pip install git+https://github.com/bokulich-lab/RESCRIPt.git # IF RESCRIPT IS NOT INSTALLED IN YOUR QIIME2 ENVIRONMENT (Last versions normally include it)
+pip install git+https://github.com/jwdebelius/q2-sidle
+qiime dev refresh-cache
+
+
+# Create Picrust2 environment for Picrust2 analysis: 
+echo -e "${Purple} Create Picrust2 environment for Picrust2 analysis... ${White}"
 #From Picrust2 tutorial: https://github.com/picrust/picrust2/wiki/PICRUSt2-Tutorial-(v2.3.0-beta)
 conda create -n picrust2 -c bioconda -c conda-forge picrust2=2.3.0_b
 
-
+# Install Metnet for functional analysis:
+echo -e "${Purple} Install Metnet for functional analysis... ${White}"
+#From tutorial: https://github.com/PlanesLab/q2-metnet
+conda activate qiime2-amplicon-2024.5
+git clone https://github.com/PlanesLab/q2-metnet.git
+cd q2-metnet/q2_metnet/
+unzip data.zip
+rm data.zip
+cd ..
+python setup.py install
+qiime dev refresh-cache
